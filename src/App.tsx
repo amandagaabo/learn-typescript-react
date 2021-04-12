79f13703a092742dc3d025fe8a97fd367b0fb814
Amanda Herschleb <amanda.herschleb@ihsmarkit.com>
2021-04-12 14:40:46 -0600

    react component with state

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
 import Message from './Message';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Message message="welcome to the site!" name="Tara" />
-      </header>
-    </div>
-  );
+
+const initialState = {
+  message: 'Typescript is cool.',
+  name: 'Beth'
+};
+
+// define types of state variables
+interface IState {
+  message: string,
+  name: string
+}
+
+// Component<props, state>
+class App extends Component<any, IState> {
+  readonly state: IState = initialState
+   render() {
+     return (
+       <div className="App">
+         <header className="App-header">
+           <img src={logo} className="App-logo" alt="logo" />
+           <Message message={this.state.message} name={this.state.name} />
+         </header>
+       </div>
+     );
+   }
 }
 
 export default App;
